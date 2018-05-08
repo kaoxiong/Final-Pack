43a3e5a82ecfc4d82f0a7d493a4e44651e74b360
kaoxiong <35321153+kaoxiong@users.noreply.github.com>
2018-05-08 09:13:45 -0500

    Update Week3script.sh

--- W3/Week3script.sh
+++ W3/Week3script.sh
@@ -1,8 +1,17 @@
 #!bin/bash
 
-#crate a device file
+clear
+echo "crate a device file"
 mknod /dev/sdal b 8 2
+sleep 2
 
-#monitor the kernel
 
+clear
+echo "monitor the kernel"
 udevadm monitor --kernel --subsystem-match=scsi
+sleep 2
+
+clear 
+echo "showing generic devices"
+lsscsi -g
+sleep 2
